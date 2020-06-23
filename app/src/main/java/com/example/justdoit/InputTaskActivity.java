eb0d78125d1a4f1e4316c401cff5758d8abc2b12
iidataiki <al18012@shibaura-it.ac.jp>
2020-06-23 16:43:51 +0900

    ×ボタンを押した時の処理を追加しました

--- app/src/main/java/com/example/justdoit/InputTaskActivity.java
+++ app/src/main/java/com/example/justdoit/InputTaskActivity.java
@@ -1,8 +1,14 @@
+/**
+ * 作成者:飯田
+ *
+ */
+
 package com.example.justdoit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class InputTaskActivity extends AppCompatActivity {
 
@@ -10,5 +16,10 @@ public class InputTaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_task);
+
+    }
+
+    public void onBackButtonClick(View view) {
+        finish();
     }
 }
