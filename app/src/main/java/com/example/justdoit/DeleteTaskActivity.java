36358129559f10a41cf01fc83babe3689fe681bd
Anzaiyasuto <al18011@shibaura-it.ac.jp>
2020-06-23 13:47:01 +0900

    変数iを作成しました

--- app/src/main/java/com/example/justdoit/DeleteTaskActivity.java
+++ app/src/main/java/com/example/justdoit/DeleteTaskActivity.java
@@ -3,17 +3,12 @@ package com.example.justdoit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class DeleteTaskActivity extends AppCompatActivity {
 
-    ArrayAdapter<String> adapter;
-    int i = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_delete_task);
     }
 }
