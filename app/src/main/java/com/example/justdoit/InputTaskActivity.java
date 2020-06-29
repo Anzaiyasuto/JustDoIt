dff3ea6fdb058c4119156ebe3eb4db81b3bb2984
iidataiki <al18012@shibaura-it.ac.jp>
2020-06-29 18:04:44 +0900

    入力制限

--- app/src/main/java/com/example/justdoit/InputTaskActivity.java
+++ app/src/main/java/com/example/justdoit/InputTaskActivity.java
@@ -8,8 +8,10 @@ package com.example.justdoit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Spanned;
 import android.view.View;
 import android.widget.EditText;
+
 
 public class InputTaskActivity extends AppCompatActivity {
 
@@ -21,6 +23,17 @@ public class InputTaskActivity extends AppCompatActivity {
         EditText edit = new EditText(this);
         edit.setHeight(50);
         setContentView(edit);
+
+        EditText edit = (EditText)findViewById(R.id.edit);
+        android.text.InputFilter[] filters = new android.text.InputFilter[1];
+        filters[0] = new android.text.InputFilter(30) {
+            @Override
+            public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+                return source;
+            }
+        };
+        edit.setFilters(filters);
+
 
     }
 
